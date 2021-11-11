736e413a32f70dd7d3e590de367c4ef0c60d86bf
AWS ParallelCluster user <ec2-user@ip-10-0-15-246.ap-southeast-1.compute.internal>
2021-11-11 06:19:20 +0000

    Prep for Quiz

--- fsall-slurm.sh
+++ fsall-slurm.sh
@@ -13,14 +13,12 @@
 #SBATCH -e fsall-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+python -u -c "import PyHipp as pyh; \
 import DataProcessingTools as DPT; \
 lfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, saveLevel=1); \
 lfall.save(); \
 hfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
-hfall.save();
+hfall.save();"
 
 aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:598928883808:awsnotify --message "FSJobDone"
-<<<<<<< HEAD
 
-=======
->>>>>>> upstream/main
